1858fd409c51ff042e8df35ccfb8dd8febf3251f
Landris18 <landry.manankoraisina.rasendranirina@esti.mg>
2021-02-05 11:50:46 +0300

    Learnig screen navigation and splashscreen with setTimeout

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React, {Component} from 'react';
-import { StyleSheet, View, Text, Image} from 'react-native';
+import { StyleSheet, View, Text, Image, Button} from 'react-native';
 import * as Font from 'expo-font';
 import { Feather } from '@expo/vector-icons';
-
 
 
 function LoadProduct() {
@@ -11,29 +12,57 @@ function LoadProduct() {
 		'Product': require('./assets/fonts/PS.ttf'),
 	});
 }
-LoadProduct()
+LoadProduct();
 
 
 
-export default class Loading extends Component {
-	render() {
-		return (
-			<View style={styles.container}>
-				<Text style={styles.hello}>TONGASOA</Text>
-			</View>
-		);
-	}
+const Stack = createStackNavigator();
+   
+
+export default function App({navigation}){
+	return(
+		<NavigationContainer>
+			<Stack.Navigator headerMode="none">
+				<Stack.Screen name="splash" component={splash}/>
+				<Stack.Screen name="home" component={home}/>	
+			</Stack.Navigator>
+		</NavigationContainer>
+	)
 }
 
 
+function  splash({navigation}){
+	setTimeout(function () {
+		navigation.navigate('home');
+	},8000);
+	return(
+		<View style={styles.container}>
+			<Text style={styles.tonga} >
+				Splash
+			</Text>
+		</View>
+	)
+}
+
+
+function home({navigation}) {
+	return (
+		<View style={styles.container}>
+			<Text style={styles.tonga}>Home</Text>
+			<Button title="Go" onPress={() => navigation.navigate('splash')}/>
+		</View>
+	)  
+}
+
 const styles = StyleSheet.create({
-	container: {
+	container:{
 		backgroundColor: '#ffffff',
 		alignItems: 'center',
 		flex: 1,
 		justifyContent: 'center',
 	},
-	hello:{
+	tonga:{
 		fontFamily: 'Product',
+		fontSize: 30,
 	}
 });
