621629b0543a3c8146054730c60c14e8d91de6c4
Landris18 <landry.manankoraisina.rasendranirina@esti.mg>
2021-02-05 14:17:28 +0300

    Splashscreen Finished

--- App.js
+++ App.js
@@ -15,9 +15,7 @@ function LoadProduct() {
 LoadProduct();
 
 
-
 const Stack = createStackNavigator();
-   
 
 export default function App({navigation}){
 	return(
@@ -31,15 +29,18 @@ export default function App({navigation}){
 }
 
 
-function  splash({navigation}){
+function splash({navigation}){
 	setTimeout(function () {
 		navigation.navigate('home');
 	},8000);
 	return(
 		<View style={styles.container}>
-			<Text style={styles.tonga} >
-				Splash
-			</Text>
+			<View style={styles.container1}>
+				<Image style={styles.logo} source={require('./assets/images/neon.png')} ></Image>
+			</View>
+			<View style={styles.container3}>
+				<Text style={styles.power}>Powered by F-Society</Text>
+			</View>
 		</View>
 	)
 }
@@ -47,7 +48,7 @@ function  splash({navigation}){
 
 function home({navigation}) {
 	return (
-		<View style={styles.container}>
+		<View style={styles.container2}>
 			<Text style={styles.tonga}>Home</Text>
 			<Button title="Go" onPress={() => navigation.navigate('splash')}/>
 		</View>
@@ -58,11 +59,40 @@ const styles = StyleSheet.create({
 	container:{
 		backgroundColor: '#ffffff',
 		alignItems: 'center',
-		flex: 1,
-		justifyContent: 'center',
+		flex: 2,
+		justifyContent:'center'
+	},
+	container1:{
+		backgroundColor: '#ffffff',
+		alignItems: 'center',
+		flex: 2,
+		justifyContent:'center',
+	},
+	container2:{
+		backgroundColor: '#ffffff',
+		alignItems: 'center',
+		flex: 2,
+		justifyContent:'center',
+	},
+	container3:{
+		position:'absolute',
+		bottom:25,
 	},
 	tonga:{
 		fontFamily: 'Product',
 		fontSize: 30,
+	},
+	logo:{
+		width:120,
+		height:120,
+		borderRadius:20,
+		shadowOffset: {width:5, height:5},
+		justifyContent:'center',
+		alignItems: 'center',
+	},
+	power:{
+		fontFamily: 'Product',
+		color: '#777',
+		fontSize: 17,
 	}
 });
