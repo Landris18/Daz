eddc5be43b0cd622599043bb5923eea4d62fe291
rootkit7628 <arleme.dev7@gmail.com>
2021-05-01 12:57:41 +0200

    add back-office test

--- back-office-node/route.js
+++ back-office-node/route.js
@@ -4,18 +4,16 @@ const dbName = require('./conf').dbName
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
 const app = express()
 const MongoClient = require('mongodb').MongoClient;
 let db
-
-async function 
+ 
 MongoClient.connect(url, function(err, client) {
   console.log("Connected successfully to server");
   db = client.db(dbName);
 });
 
-app.get('/api/user', (req,res) => {
+app.get('/api/users', (req,res) => {
     db.collection('User').find({}).toArray(function(err, docs) {
         if (err) {
             console.log(err)
@@ -26,8 +24,16 @@ app.get('/api/user', (req,res) => {
 })
 
 app.listen(3000, () => {
- console.log('Go to http://localhost:3000/users so you can see the data.');
+ console.log('Go to http://localhost:3000/users');
 });
 
 
-
+async function login({ username, password }) {
+    const user = await db.collection('User').findOne({ username });
+    if (user && bcrypt.compareSync(password, user.hash)) {
+        return {
+            ...user.toJSON(),
+            token
+        };
+    }
+}
