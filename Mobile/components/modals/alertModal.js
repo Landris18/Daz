450197274995a11f7725eb0790175552ca41fb12
Landris18 <landry.manankoraisina.rasendranirina@esti.mg>
2021-05-15 22:38:48 +0300

    Creating custom alert (modal)

---
+++ Mobile/components/modals/alertModal.js
@@ -0,0 +1,36 @@
+// //Importations des modules et des components
+// import 'react-native-gesture-handler';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import React, { Component } from 'react';
+// // import styles from './assets/css/stylesheets';
+
+import { Keyboard,View, Text, Image, ScrollView, Alert, ImageBackground} from 'react-native';
+import Modal from 'react-native-modal';
+import styles from '../../assets/css/css';
+// import * as Font from 'expo-font';
+import { Ionicons,FontAwesome5, Entypo, MaterialIcons, AntDesign, MaterialCommunityIcons} from '@expo/vector-icons';
+// import { TextInput } from 'react-native-gesture-handler';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+export class AlertModal extends Component{
+     render(){
+          return(
+               <Modal 
+               isVisible={this.props.isVisible} 
+               animationInTiming={1000}
+               animationOutTiming={1} 
+               animationIn={'shake'}
+               animationOut={'swing'}
+               backdropOpacity={0.80}
+               style={{alignItems:'center'}}>
+                    <View style={styles.modalView}>
+                         <MaterialCommunityIcons name="lock-alert" size={60} color="#999" style={{marginTop:hp('-2%')}} />
+                         <Text style={styles.modalText}>You inserted a wrong password !</Text>
+                         <Text onPress={() => {this.props.action()}} style={styles.btnModal}> Valider</Text>
+                    </View>
+               </Modal>
+          )
+     }
+}
